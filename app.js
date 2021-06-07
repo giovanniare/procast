a668b91bc57209f6df6d0b882d48bbb113621618
Giovanni <giovanniare9525@gmail.com>
2021-06-07 17:36:35 -0500

    first desing for procast

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const hbs = require('hbs')
+
+require('./hbs/helpers')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`)
+})
