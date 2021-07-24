d11359e1634f1ca3086e688395411676cf5859b1
Giovanni <giovanniare9525@gmail.com>
2021-07-23 22:42:03 -0500

    cambios adicionales 1

--- app.js
+++ app.js
@@ -9,10 +9,44 @@ const port = process.env.PORT || 3000
 app.use(express.static(__dirname + '/public'))
 
 hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerPartials(__dirname + '/views/partials_new')
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', {
+        imageHeader: '../../assets/img/masthead.jpg',
+        tittle: 'PROCAST'
+    })
+})
+
+app.get('/desarrollos', (req, res) => {
+    res.render('desarrollos', {
+        imageHeader: '../../assets/img/desarrollo.jpg',
+        tittle: 'DESARROLLOS',
+        descripcion: 'En Procast la Arquitectura es un todo, por esto en nuestro proceso entendemos la visión de 360 grados con arquitectos, estructurístas, financieros, constructores, diseñadores industriales y gráficos los cuales agregan valor y garantizan la ejecución de un proyecto integral.'
+    })
+})
+
+app.get('/propiedades', (req, res) => {
+    res.render('propiedades', {
+        imageHeader: '../../assets/img/propiedades.jpg',
+        tittle: 'PROPIEDADES',
+        descripcion: 'Da un vistazo a nuestro catalogo de propiedades listas para ti.'
+    })
+})
+
+app.get('/blog', (req, res) => {
+    res.render('blog', {
+        imageHeader: '../../assets/img/blog.jpg',
+        tittle: 'PROXIMAMENTE',
+        descripcion: 'Espera nuestro nuevo blog, aqui oodras aprender sobre las tendencias y mejores practicas sobre real estate.'
+    })
+})
+
+// DESAROLLOS
+
+app.get('/desarrollos/casa-kh', (req, res) => {
+    res.render('desarrollos')
 })
 
 app.listen(port, () => {
--- hbs/helpers.js
+++ hbs/helpers.js
@@ -3,3 +3,7 @@ const hbs = require('hbs')
 hbs.registerHelper('getAnio', () => {
     return new Date().getFullYear()
 })
+
+hbs.registerHelper('getHeader', (info) => {
+    return info
+})
